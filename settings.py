22ce5ae36be6c98da72ac28cbfc13f2d8a81ab13
Sergey Leschina <mail@putnik.ws>
2012-06-18 05:38:28 +0400

    Fix import for flatpages

--- settings.py
+++ settings.py
@@ -122,12 +122,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
+    'django.contrib.flatpages',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'flatpages',
     'tinymce',
     'yafotki',
 
