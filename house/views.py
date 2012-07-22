5143deb064f5e33bd723cd4c7546e9e550b57d15
Sergey Leschina <mail@putnik.ws>
2012-07-22 18:15:09 +0400

    Sort houses better

--- house/views.py
+++ house/views.py
@@ -29,7 +29,7 @@ def index_page(request):
 def street(request, id):
     s = Street.objects.get(pk=id)
     s_list = Street.objects.order_by('name').annotate(house_count=Count('house'))
-    h_list = House.objects.filter(street=id).order_by('number')
+    h_list = House.objects.filter(street=id).extra(select={'number_int': 'LPAD(IF(number REGEXP "[0-9]$", CONCAT(number, " "), number), 6, " ")'}).order_by('number_int')
     map_border = h_list.aggregate(Max('coord_lon'), Min('coord_lon'), Max('coord_lat'), Min('coord_lat'))
     if map_border['coord_lat__max'] != None:
         map_center = {
